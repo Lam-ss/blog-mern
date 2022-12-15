0a86f67d9248035a24a35202f2acfc47b3e9f01a
worker <workerpo4ta@gmail.com>
2022-12-15 11:47:06 +0300

    connect mongoDB

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
 import express from "express";
 import jwt from "jsonwebtoken";
+import mongoose from "mongoose";
+
+mongoose
+  .connect(
+    "mongodb+srv://admin:55555@cluster0.irmuytj.mongodb.net/?retryWrites=true&w=majority"
+  )
+  .then(() => console.log("DB ok"))
+  .catch((err) => console.log("DB error", err));
 
 const app = express();
 
@@ -12,13 +20,15 @@ app.get("/", (req, res) => {
 app.post("/auth/login", (req, res) => {
   console.log(req.body);
 
-  const token = jwt.sign(
-    {
-      email: req.body.email,
-      fullName: "Vasia",
-    },
-    "secret123"
-  );
+  if (req.body.email === "test@text.ru") {
+    const token = jwt.sign(
+      {
+        email: req.body.email,
+        fullName: "Вася пупкин",
+      },
+      "secret123"
+    );
+  }
 
   res.json({
     seccess: true,
