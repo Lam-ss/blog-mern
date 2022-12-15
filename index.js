d126c43ca7b586cf1c1137b05e27dfcf1af0829f
worker <workerpo4ta@gmail.com>
2022-12-15 16:10:53 +0300

    validator

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 import express from "express";
 import jwt from "jsonwebtoken";
 import mongoose from "mongoose";
+import { validationResult } from "express-validator";
+import { registerValidation } from "./validations/auth.js";
 
 mongoose
   .connect(
@@ -13,26 +15,14 @@ const app = express();
 
 app.use(express.json());
 
-app.get("/", (req, res) => {
-  res.send("111hello");
-});
-
-app.post("/auth/login", (req, res) => {
-  console.log(req.body);
-
-  if (req.body.email === "test@text.ru") {
-    const token = jwt.sign(
-      {
-        email: req.body.email,
-        fullName: "Вася пупкин",
-      },
-      "secret123"
-    );
+app.post("/auth/register", registerValidation, (req, res) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json(errors.array());
   }
 
   res.json({
-    seccess: true,
-    token,
+    success: true,
   });
 });
 
